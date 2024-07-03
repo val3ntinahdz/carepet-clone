71550b3420bf7d88069615cecdbd67c4c075098c
Donovan Montiel <dmontieloliveros@gmail.com>
2024-07-02 19:26:38 -0600

    Avance de navbar, menú, vistas y controller de pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,49 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: %i[show edit update destroy]
+
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @pet.update(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to user_pet_path
+  end
+
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :pet_type, :breed, :birthday_date, :gender, :weight, :height, :color)
+  end
 end
