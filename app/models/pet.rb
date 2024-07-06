8331a911e71340bddc6df2c742e78c85333c4592
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2024-07-06 12:24:57 -0600

    Load to cloudinary the AI generated photo

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class Pet < ApplicationRecord
   belongs_to :user
   has_many :vaccinations
@@ -11,20 +13,26 @@ class Pet < ApplicationRecord
   has_many :nutritions
   has_many :allergies
   has_many :allergens, through: :allergies
-  # after_save :set_photo
-  # private
-  # def set_photo
-  #   prompt = <<-PROMPT
-  #   Create a high resolution and high-quality Pixar 3D animated film style image of a #{color} #{breed} in nature.
-  #   Capture the essence of the scene with detailed rendering and lifelike textures.
-  #   The #{breed} should have a realistic build and expressive eyes, with a coat that accurately represents the breed's characteristics.
-  #   With a vibrant and dynamic natural background featuring greenery, trees, and a blue sky.
-  #   PROMPT
-  #   client = OpenAI::Client.new
-  #   response = client.images.generate(parameters: { prompt: prompt })
-  #   url = response["data"][0]["url"]
-  #   file =  URI.open(url)
-  #   photo.attach(io: file, filename: "#{id}_#{name}_pet.jpg", content_type: "image/png")
-  #   return photo
-  # end
+  has_one_attached :photo
+  before_create :set_photo
+
+  private
+
+  def set_photo
+    # puts
+    # puts
+    # puts " == Generating photo! =="
+    prompt = <<-PROMPT
+    Create a high resolution and high-quality Pixar 3D animated film style image of a #{color} #{breed} in nature.
+    Capture the essence of the scene with detailed rendering and lifelike textures.
+    The #{breed} should have a realistic build and expressive eyes, with a coat that accurately represents the breed's characteristics.
+    With a vibrant and dynamic natural background featuring greenery, trees, and a blue sky.
+    PROMPT
+    client = OpenAI::Client.new
+    response = client.images.generate(parameters: { prompt: prompt })
+    url = response["data"][0]["url"]
+    file =  URI.open(url)
+    photo.attach(io: file, filename: "#{id}_#{name}_pet.jpg", content_type: "image/png")
+    return photo
+  end
 end
