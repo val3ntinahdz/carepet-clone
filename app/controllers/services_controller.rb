466f6dbe4b15c91ea4d6c320e0949b2eba60e385
your_github_username <valentinaleonh8@gmail.com>
2024-07-13 12:49:08 -0600

    services adjustments

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,28 +1,10 @@
 class ServicesController < ApplicationController
 
   def index
-    if params[:query].present?
-      @services = Service.search_by(params[:query])
-      @veterinaries = @services.map { |service| service.veterinary.user }
-      @markers = @veterinaries.map do |veterinary|
-        {
-          lat: veterinary.latitude,
-          lng: veterinary.longitude,
-          info_window_html: render_to_string(partial: "info_window", locals: {veterinary: veterinary}),
-          marker_html: render_to_string(partial: "marker", locals: { type: :veterinary })
-        }
-      end
+    if current_user.veterinary.present?
+      @services = current_user.veterinary.services
     else
-      @services = Service.all
-    end
-
-    if current_user.latitude.present? && current_user.longitude.present?
-      @markers << {
-        lat: current_user.latitude,
-        lng: current_user.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: { user: current_user }),
-        marker_html: render_to_string(partial: "marker", locals: { type: :user })
-      }
+      render_map_and_search_results
     end
   end
 
@@ -41,13 +23,42 @@ class ServicesController < ApplicationController
     @service.veterinary_id = params[:veterinary_id]
 
     if @service.save
-      redirect_to service_path(@service)
+      redirect_to services_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   private
+
+  def render_map_and_search_results
+    @veterinaries = []
+    
+    if params[:query].present?
+      @services = Service.search_by(params[:query])
+      @veterinaries = @services.map { |service| service.veterinary.user }
+    else
+      @services = Service.all
+    end
+
+    @markers = @veterinaries.map do |veterinary|
+      {
+        lat: veterinary.latitude,
+        lng: veterinary.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {veterinary: veterinary}),
+        marker_html: render_to_string(partial: "marker", locals: { type: :veterinary })
+      }
+    end
+
+    if current_user.latitude.present? && current_user.longitude.present?
+      @markers << {
+        lat: current_user.latitude,
+        lng: current_user.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { user: current_user }),
+        marker_html: render_to_string(partial: "marker", locals: { type: :user })
+      }
+    end
+  end
 
   def service_params
     params.require(:service).permit(:name, :category, :description, :fee)
