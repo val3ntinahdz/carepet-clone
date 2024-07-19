12e38b733b54f763b38cddd5d3c3a2c87e374dce
your_github_username <valentinaleonh8@gmail.com>
2024-07-18 20:45:35 -0600

    calendar tests

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -3,15 +3,22 @@ import flatpickr from "flatpickr"
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
+    static values = { bookedDates: Array }
 
-  connect() {
-    console.log("connected to flatpickr", this.element)
+    connect() {
+      console.log("connected to flatpickr", this.element)
 
-    flatpickr((".datetime"), {
-      enableTime: true,
-      dateFormat: "Y-m-d H:i",
-      minDate: "today",
-      disableMobile: true
-    });
+      // const bookedDates = JSON.parse(this.element.dataset.bookedDates);
+      // const disabledDates = bookedDates.map(date => new Date(date));
+      const disabledDates = this.bookedDatesValue.map(date => new Date(date));
+
+      flatpickr(this.element.querySelector(".datetime"), {
+        enableTime: true,
+        dateFormat: "Y-m-d H:i",
+        minDate: "today",
+        disableMobile: true,
+        time_24hr: true,
+        disable: [ function(date) { return disabledDates.some(bookedDate => { return ( date.getFullYear() === bookedDate.getFullYear() && date.getMonth() === bookedDate.getMonth() && date.getDate() === bookedDate.getDate() && date.getHours() === bookedDate.getHours() && date.getMinutes() === bookedDate.getMinutes() ); }); } ]
+      });
   }
 }
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,16 +3,36 @@ class Appointment < ApplicationRecord
   belongs_to :service
 
   validates :reason, :datetime, presence: true
+  validate :datetime_cannot_be_in_the_past
+  validate :datetime_must_be_available
 
-  def end_time
-    datetime + 1.hour
+  # def end_time
+  #   datetime + 1.hour
+  # end
+
+  # def time
+  #   "#{datetime.strftime('%I:%M %p')} - #{end_time.strftime('%I:%M %p')}"
+  # end
+
+  # def multi_days?
+  #   (end_time.to_date - datetime.to_date).to_i >= 1
+  # end
+
+  def datetime_cannot_be_in_the_past
+    errors.add(:datetime, "can't be in the past") if datetime < DateTime.now
   end
 
-  def time
-    "#{datetime.strftime('%I:%M %p')} - #{end_time.strftime('%I:%M %p')}"
-  end
-
-  def multi_days?
-    (end_time.to_date - datetime.to_date).to_i >= 1
+  def datetime_must_be_available
+    # if Appointment.exists?(datetime: datetime, service_id: service_id)
+    #   errors.add(:datetime, 'is not available')
+    # end
+    appointment_duration = 1.hour
+    appointment_start_time = datetime
+    appointment_end_time = datetime + appointment_duration
+    overlapping_appointments = Appointment.where(service_id: service_id)
+                                          .where('datetime < ? AND ? < datetime + interval ?', appointment_end_time, appointment_start_time, '1 hour')
+    if overlapping_appointments.exists?
+      errors.add(:datetime, 'is not available')
+    end
   end
 end
