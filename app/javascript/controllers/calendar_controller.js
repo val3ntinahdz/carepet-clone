a9df9b5237922e37c34ca31965430ca13b202434
your_github_username <valentinaleonh8@gmail.com>
2024-07-20 16:20:17 -0600

    calendar back-end and stimulus

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -3,14 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="calendar"
 export default class extends Controller {
   static values = { dayNumber: String }
-  static targets = ["form", "hour"]
+  static targets = ["form", "hour", "input", "savedDate"]
 
   connect() {
-    console.log("connected to calendar")
   }
 
   openModal() {
-    console.log(this.dayNumberValue)
     this.formTarget.classList.remove("d-none")
     this.formTarget.classList.add("calendar-modal")
   }
@@ -20,7 +18,22 @@ export default class extends Controller {
     this.hourTargets.forEach(hour => {
       hour.classList.remove("btn-primary")
     });
-    
+
     event.currentTarget.classList.add("btn-primary")
+    const tiempo = event.currentTarget.dataset.tiempo
+    const fecha = event.currentTarget.dataset.fecha
+    const selectedTimeElement = document.getElementById("selected_time")
+    const month = parseInt(event.currentTarget.dataset.month) - 1
+    const selectedDate = new Date (2024, month, fecha, tiempo, 0, 0).toString()
+    this.savedDateTarget.innerText = selectedDate
+    console.log(selectedDate)
+
+    this.inputTarget.value = selectedDate
+    console.log(this.inputTarget.value)
+  }
+
+  closeModal() {
+    this.formTarget.classList.add("d-none")
+    this.formTarget.classList.remove("calendar-modal")
   }
 }
--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -6,19 +6,19 @@ export default class extends Controller {
     static values = { bookedDates: Array }
 
     connect() {
-      console.log("connected to flatpickr", this.element)
+      // console.log("connected to flatpickr", this.element)
 
-      // const bookedDates = JSON.parse(this.element.dataset.bookedDates);
-      // const disabledDates = bookedDates.map(date => new Date(date));
-      const disabledDates = this.bookedDatesValue.map(date => new Date(date));
+      // // const bookedDates = JSON.parse(this.element.dataset.bookedDates);
+      // // const disabledDates = bookedDates.map(date => new Date(date));
+      // const disabledDates = this.bookedDatesValue.map(date => new Date(date));
 
-      flatpickr(this.element.querySelector(".datetime"), {
-        enableTime: true,
-        dateFormat: "Y-m-d H:i",
-        minDate: "today",
-        disableMobile: true,
-        time_24hr: true,
-        disable: [ function(date) { return disabledDates.some(bookedDate => { return ( date.getFullYear() === bookedDate.getFullYear() && date.getMonth() === bookedDate.getMonth() && date.getDate() === bookedDate.getDate() && date.getHours() === bookedDate.getHours() && date.getMinutes() === bookedDate.getMinutes() ); }); } ]
-      });
+      // flatpickr(this.element.querySelector(".datetime"), {
+      //   enableTime: true,
+      //   dateFormat: "Y-m-d H:i",
+      //   minDate: "today",
+      //   disableMobile: true,
+      //   time_24hr: true,
+      //   disable: [ function(date) { return disabledDates.some(bookedDate => { return ( date.getFullYear() === bookedDate.getFullYear() && date.getMonth() === bookedDate.getMonth() && date.getDate() === bookedDate.getDate() && date.getHours() === bookedDate.getHours() && date.getMinutes() === bookedDate.getMinutes() ); }); } ]
+      // });
   }
 }
