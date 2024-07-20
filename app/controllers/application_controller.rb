ec63db1e783c83c9946ac4d64275f5a5a4d21e88
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2024-07-20 13:43:23 -0600

    Update user profile photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :phone_number])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :phone_number, :photo])
   end
 
   def after_sign_in_path_for(resource)
