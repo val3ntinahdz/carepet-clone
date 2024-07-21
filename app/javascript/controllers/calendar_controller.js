3255214c1cdb24ccf751a3159cb84c2d0e778693
your_github_username <valentinaleonh8@gmail.com>
2024-07-21 16:51:10 -0600

    stimulus calendar

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
   static targets = ["form", "hour", "input", "savedDate"]
 
   connect() {
+    this.closeTimeout = null
   }
 
   openModal() {
@@ -24,12 +25,23 @@ export default class extends Controller {
     const fecha = event.currentTarget.dataset.fecha
     const selectedTimeElement = document.getElementById("selected_time")
     const month = parseInt(event.currentTarget.dataset.month) - 1
-    const selectedDate = new Date (2024, month, fecha, tiempo, 0, 0).toString()
-    this.savedDateTarget.innerText = selectedDate
+    const selectedDate = new Date (2024, month, fecha, tiempo, 0, 0)
+    const selectedDateString = selectedDate.toLocaleString()
     console.log(selectedDate)
 
+    this.savedDateTarget.innerText = selectedDateString
     this.inputTarget.value = selectedDate
     console.log(this.inputTarget.value)
+  }
+
+  startCloseModalTimer() {
+    this.closeTimeout = setTimeout(() => {
+      this.closeModal()
+    }, 7000)
+  }
+
+  cancelCloseModalTimer() {
+    clearTimeout(this.closeTimeout)
   }
 
   closeModal() {
